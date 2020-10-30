7ef88bb2aa590e50e72b4ee71c09f8ff04a4138f
haveeru <suood_mohamed@hotmail.com>
2020-10-30 07:43:36 -0700

    Bringing redux state into homeScreen 28

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 // applyMiddleware can be used to impliment
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducer } from './reducers/productReducers';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+    productList: productListReducer
+});
 
 const initialState = {};
 
