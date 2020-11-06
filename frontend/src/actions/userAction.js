615dbb3f8745796653b555fd5dbe759e7ce57317
haveeru <suood_mohamed@hotmail.com>
2020-11-06 22:36:39 +0500

    show user in navbar and logout

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -3,7 +3,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
-  //USER_LOGOUT,
+  USER_LOGOUT,
 } from '../constants/userConstants';
 
 // redux thunk is used here
@@ -41,4 +41,9 @@ export const login = (email, password) => async (dispatch) => {
           : error.message,
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
 };
