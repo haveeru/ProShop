5a288eb2ca122bc3bf8ccd72d7247d9de5aae188
haveeru <suood_mohamed@hotmail.com>
2020-11-07 06:27:08 +0500

    fix display error message in register screen

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -35,6 +35,7 @@ const RegisterScreen = ({ location, history }) => {
     <FormContainer>
       <h1>Sign Up</h1>
       {message && <Message variant="danger">{message}</Message>}
+      {error && <Message variant='danger'>{error}</Message>}
       {loading && <Loader />}
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="name">
